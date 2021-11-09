a84f3af36f3d376ced30ef687020aaa75b583115
daisukeyamaguchi329 <gaman55894@gmail.com>
2021-11-09 14:51:01 +0000

    Uers2 Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#top'
   resource :user, only: [:new, :create, :show]
+  get 'login', to: "sessions#new"
+  post 'login',  to: "sessions#create"
 end
+aaaa
